29b5546c18da1f2e26e32236e8c8542a8345841a
ZeNiTH-P0k3R <azn_puhsuazn@live.com>
2025-01-04 20:20:43 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const cacheName = "randomizer-v1";
+const assets = [
+  "./index.html",
+  "./manifest.json",
+  "./icon.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
